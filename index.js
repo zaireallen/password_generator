41cbeb70279a6a9af554e018ed50cf348f868eb6
Zai <allenzaire@gmail.com>
2025-04-18 17:27:17 -0400

    Got the function to work so we can now copy to clipboard when a new password is generated. I need to work on my alert. I'd like a better feature than the current alert. I would like a toast. As well as that, I need to now work on displaying elements once other things are enabled.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let gen_pword_button = document.getElementsByClassName("generate-button")
+let gen_pword_button = document.getElementById("generate-button")
 let pword_output = document.getElementById("generatedPWord")
 
 let copy_btn = document.getElementById("copy-button")
@@ -23,5 +23,9 @@ function genPword () {
 // copy button
 
 copy_btn.onclick = function () {
-    pword_output.select()
+    // pword_output.select()
+    let val = document.getElementById('generatedPWord').value;
+    navigator.clipboard.writeText(val).then(function (){
+        alert("Text copied")
+    })
 }
